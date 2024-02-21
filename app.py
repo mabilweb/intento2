d52118e259b2d4511a6a583b12d5f9cf530ab30b
mabilweb <h3mabil@gmail.com>
2024-02-21 16:35:39 -0600

    elcheckbox

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+st.header('Mi primera aplicación', divider='rainbow')
 
 car_data = pd.read_csv(open("vehicles_us.csv")) # leer los datos
 
@@ -22,6 +23,13 @@ if hist_button: # al hacer clic en el botón
         
 if hist_button2: # al hacer clic en el botón
             # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un diagrama de dispersión para el conjunto de datos de anuncios de venta de coches')
     fig = px.scatter(car_data, x="odometer",y='price')
     st.plotly_chart(fig, use_container_width=True) 
+
+
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
+    fig = px.histogram(car_data, x="odometer")
